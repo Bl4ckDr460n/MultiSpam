1943679f8b9cef460c7ebc3280b901992ee0cbf2
Bl4ck_Dr460n <49341995+Bl4ckDr460n@users.noreply.github.com>
2019-04-19 06:55:29 +0700

    Add files via upload

---
+++ MultiSpam.sh
@@ -0,0 +1,142 @@
+#system/bin/!
+# Jangan Recode Bossq
+# Klo Recode Selain admin Gw Sumpahin Lo Akan Mati!!!!!!!
+# wkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwwkwkkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkekekwkekwkwkwkwkwkwkwkwkwkwkwkekekwkwkwkwk
+
+# Warna
+biru='\033[34;1m' #biru
+green='\e[0;32m' #hijau muda
+daun='\033[32;1m' #ijo
+purple='\033[35;1m' #purple
+cyyan='\033[36;1m' #cyan
+merah='\033[31;1m' #merah
+putih='\033[37;1m' #pur
+tai='\033[33;1m' #kuning
+
+# Mulai Tools
+clear
+echo $tai
+toilet -f slant --gay "Multi Spam"
+sleep 1
+echo
+echo
+echo $daun "Tools By Bl4ck_Dr460n"
+sleep 1
+echo $tai "Gunakan Dgn Bijak Ya Bossq"
+sleep 1
+echo
+echo $putih "MENU :"
+echo
+echo $merah "[]================[]"
+echo $daun  "  1.Bukalapak"
+sleep 0.4
+echo $merah "[]================[]"
+echo $daun  "  2.Telkomsel"
+sleep 0.4
+echo $merah "[]================[]"
+echo $daun  "  3.Grab  "
+sleep 0.4
+echo $merah "[]================[]"
+echo $daun  "  4.TokoPedia"
+echo $merah "[]================[]"
+sleep 1
+echo
+echo $merah "[]================[]"
+echo $daun  "  0.Install Bahan"
+echo $merah "[]================[]"
+sleep 0.4
+echo $daun  "  E.Exit"
+echo $merah "[]================[]"
+echo $daun
+read -p  "[root@dr460n]>> " pilih
+if [ $pilih = "1" ] || [ $pilih = "1" ]
+then
+clear
+echo $daun
+toilet -f slant --gay "BUKALAPAK"
+sleep 1
+echo $daun "Code By Bl4ck_Dr460n"
+sleep 1
+echo $merah "Gunakan Dgn Bijak Ya Bossq"
+sleep 1
+echo
+echo
+cd php
+php 1.php
+exit
+fi
+if [ $pilih = "2" ] || [ $pilih = "2" ]
+then
+clear
+echo $daun
+toilet -f slant --gay "TSEL"
+sleep 1
+echo $daun "Code By Bl4ck_Dr460n"
+sleep 1
+echo $merah "Gunakan Dgn Bijak Ya Bossq"
+sleep 1
+echo
+echo
+cd php
+php 2.php
+exit
+fi
+if [ $pilih = "3" ] || [ $pilih = "3" ]
+then
+clear
+echo $daun
+toilet -f slant --gay "GRAB"
+sleep 1
+echo $daun "Code By Bl4ck_Dr460n"
+sleep 1
+echo $merah "Gunakan Dgn Bijak Ya Bossq"
+sleep 1
+echo
+echo
+cd php
+php 3.php
+exit
+fi
+if [ $pilih = "4" ] || [ $pilih = "4" ]
+then
+clear
+echo $daun
+toilet -f slant --gay "TokPed"
+sleep 1
+echo $daun "Code By Bl4ck_Dr460n"
+sleep 1
+echo $merah "Gunakan Dgn Bijak Ya Bossq"
+sleep 1
+echo
+echo
+cd php
+php 4.php
+exit
+fi
+if [ $pilih = "0" ] || [ $pilih = "0" ]
+then
+clear
+echo $daun
+toilet -f slant --gay "INSTALL BAHAN"
+sleep 1
+echo $daun "Code By Bl4ck_Dr460n"
+sleep 1
+echo $merah "Gunakan Dgn Bijak Ya Bossq"
+sleep 1
+echo
+echo
+cd dll
+chmod +x *
+./install
+sleep 4
+fi
+if [ $pilih = "E" ] || [ $pilih = "e" ]
+then
+echo $merah "Author = Bl4ck Dr460n"
+sleep 1
+echo $daun "Whatsapp = 089517248xxx"
+sleep 1
+echo $tai "Tunggu 5 Detik"
+sleep 5
+exit
+fi
